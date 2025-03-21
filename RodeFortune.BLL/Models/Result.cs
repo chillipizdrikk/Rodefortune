c95f9923e03a9103631bc2a88c8e0a3c2a2f9b88
midnightpretenders <125821824+midnightpretenders@users.noreply.github.com>
2025-03-21 10:21:54 +0200

    Added class Result.cs for messaging the errors

---
+++ RodeFortune.BLL/Models/Result.cs
@@ -0,0 +1,27 @@
+﻿namespace RodeFortune.BLL.Models
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+
+        public Result(bool success, string message, T? data = default)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+    }
+}
